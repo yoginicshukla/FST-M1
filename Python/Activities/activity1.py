a6de5823265a36aa4d66c4f4b27409f209c9a3b1
yoginicshukla <132568744+yoginicshukla@users.noreply.github.com>
2023-05-19 11:39:10 +0530

    Add files via upload

---
+++ Python/Activities/activity1.py
@@ -0,0 +1,4 @@
+name=input("what is your name:")
+age=int(input("how old are you:"))
+year=str((2023-age)+100)
+print(name+"will be be 100 ears ord in the year"+year)
---
+++ Python/Activities/activity2.py
@@ -0,0 +1,7 @@
+num=int(input("Enter number:"))
+mod=num%2
+if mod>0:
+    print("You picked an odd number")
+else:
+    print("you picked an even number")
+
